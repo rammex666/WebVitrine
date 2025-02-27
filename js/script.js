f2709473c8d95e24ebedcce4124501c2c9ca3769
.rammex <104463176+rammex666@users.noreply.github.com>
2025-02-27 16:30:20 +0100

    data numbers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-// Déclaration de la fonction
-function afficherMessage() {
-    let message = "Bienvenue sur notre site web!";
-    alert(message); // Affiche le message dans une alerte
+let visits = 0;
+
+function addVisit() {
+    visits++;
+    console.log(visits);
 }
+
+function afficherNumber(number, element, message, duration){
+    let a = 0;
+    let time = duration / number ;
+    if(message === undefined){
+        message = "";
+    }
+    function incrementAndDisplay() {
+        if (a < number) {
+            a++;
+            document.getElementById(element).innerText = a + " " + message;
+            setTimeout(incrementAndDisplay, time);
+        }
+    }
+    incrementAndDisplay();
+}
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    addVisit();
+    afficherNumber(visits);
+});*/
+
+
+afficherNumber(10000, "customers", "Customers", 3);
+afficherNumber(1000, "sold", "Sale", 3000);
+afficherNumber(1000, "ca", "$", 3000);
+
+
+
